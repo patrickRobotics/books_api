b862497af832be27aa597a3be3bbc367d75d8b82
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 <pirungu9@gmail.com>
2024-12-26 22:31:25 +0300

    Add relationship links

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,6 @@
 class BookSerializer < ApplicationSerializer
   attributes :title, :language_code, :num_pages
+  link(:self) { book_path(object)}
+  link(:authors) {authors_path(book_id: object.id)}
   has_many :authors
 end

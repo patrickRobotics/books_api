f6f8f3497df58c1c8fe2ff5458d0520a94e3030b
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 <pirungu9@gmail.com>
2024-12-28 00:09:11 +0300

    Filtering and Sorting

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ['name']
+  end
   has_and_belongs_to_many :books
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,8 @@ class Book < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ['title', 'author_name']
   end
+  def self.ransackable_associations(auth_object = nil)
+    ['authors']
+  end
   has_and_belongs_to_many :authors
 end
